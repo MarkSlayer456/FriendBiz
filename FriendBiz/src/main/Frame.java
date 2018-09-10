c188f556f350c3ebcb2359fef9052a501ee7dc37
MarkSlayer456 <MarkSlayer456@DESKTOP-4K7FNFP>
2018-09-10 00:16:05 -0400

    Fixed Frame.java

--- FriendBiz/src/main/Frame.java
+++ FriendBiz/src/main/Frame.java
@@ -5,16 +5,20 @@ import javax.swing.JFrame;
 public class Frame {
 
 	
-	private static JFrame jFrame = new JFrame("FriendBiz"); // The actual frame
-	public static Frame frame = new Frame(jFrame); // Only frame that will be used throughout the program
+	private static JFrame javaFrame = new JFrame("FriendBiz"); // The actual frame
+	public static Frame frame = new Frame(javaFrame); // Only frame that will be used throughout the program
+	private JFrame jf;
 	
-	
-	public Frame(JFrame jf) { // Creates the one and only frame this program will use
-		jFrame.setBounds(400, 150, 800, 600); // TODO change this later
-		jFrame.setVisible(true);
-		jFrame.setDefaultCloseOperation(1); // Closes the window
-		//jFrame.setIconImage(image); // TODO add icon
+	public Frame(JFrame jFrame) { // Creates the one and only frame this program will use
+		jf = jFrame;
+		jf.setBounds(400, 150, 800, 600); // TODO change this later
+		jf.setDefaultCloseOperation(1); // Closes the window
+		//jf.setIconImage(image); // TODO add icon
 		
+	}
+	
+	public JFrame getFrame() {
+		return this.jf;
 	}
 	
 	public static Frame getWindow() { // I like to call them windows not frames!
@@ -22,7 +26,9 @@ public class Frame {
 	}
 	
 	public static void main(String[] args) {
-		return;
+		System.out.println("Showing screen...");
+		getWindow().jf.setVisible(true);
+		
 	}
 	
 }
