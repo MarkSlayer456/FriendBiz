1dc2e0c1537788e691dc51eb7f5ea70a9c4cee94
MarkSlayer456 <MarkSlayer456@DESKTOP-4K7FNFP>
2018-09-10 00:11:45 -0400

    Add a text file, and created a test for the frame will update later!

--- FriendBiz/src/main/Frame.java
+++ FriendBiz/src/main/Frame.java
@@ -1,12 +1,28 @@
 package main;
 
+import javax.swing.JFrame;
+
 public class Frame {
 
-	public static int test;
+	
+	private static JFrame jFrame = new JFrame("FriendBiz"); // The actual frame
+	public static Frame frame = new Frame(jFrame); // Only frame that will be used throughout the program
+	
+	
+	public Frame(JFrame jf) { // Creates the one and only frame this program will use
+		jFrame.setBounds(400, 150, 800, 600); // TODO change this later
+		jFrame.setVisible(true);
+		jFrame.setDefaultCloseOperation(1); // Closes the window
+		//jFrame.setIconImage(image); // TODO add icon
+		
+	}
+	
+	public static Frame getWindow() { // I like to call them windows not frames!
+		return frame;
+	}
 	
 	public static void main(String[] args) {
-		test = 5;
-		System.out.println(test);
+		return;
 	}
 	
 }
