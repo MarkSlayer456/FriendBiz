e8a9d5018b9dcd7521cf34c17ddb338595aff3e2
MarkSlayer456 <MarkSlayer456@DESKTOP-4K7FNFP>
2018-09-10 09:38:59 -0400

    Added a quitApp method and added a line to start the internal clock

--- FriendBiz/src/main/Frame.java
+++ FriendBiz/src/main/Frame.java
@@ -11,12 +11,16 @@ public class Frame {
 	//TODO get screen size and adjust frame size to it
 	
 	
+	@SuppressWarnings("static-access") //TODO Can run without this but an error was annoying so it's here
 	public Frame(JFrame jFrame) { // Creates the one and only frame this program will use
 		jf = jFrame;
 		jf.setBounds(400, 150, 800, 600); // TODO change this later
-		jf.setDefaultCloseOperation(1); // Closes the window
+		jf.setDefaultCloseOperation(jf.EXIT_ON_CLOSE); // Closes the window
 		//jf.setIconImage(image); // TODO add icon
-		
+	}
+	
+	public void quitApp() { // Used to close the application at any time
+		jf.dispose();
 	}
 	
 	public JFrame getFrame() { // Gets the JFrame no the frame object I created
@@ -28,10 +32,13 @@ public class Frame {
 	}
 	
 	public static void main(String[] args) {
+		Time time = new Time();
+		time.start();
 		System.out.println("Showing screen..."); //TODO remove this
 		getWindow().jf.setVisible(true); //TODO move to setup method in main
 		Thread main = new Thread(Main.main);
 		main.start();
+		
 		
 	}
 	
